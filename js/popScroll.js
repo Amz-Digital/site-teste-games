5e5a425f8591294257ea1b955065489011874fd9
renaniomes <renaniomes10@gmail.com>
2022-09-27 13:54:13 -0300

    att 27.09.2022

--- js/popScroll.js
+++ js/popScroll.js
@@ -1,35 +1,60 @@
-function mostrarPopUp(){
-  var botao = document.getElementById('botaoPop');
-  botao.click();
+function mostrarPopUp()
+{
+  mostrarPop = true;
+  mostrarPop = localStorage.getItem("mostrarPop");
+  if (mostrarPop)
+  {
+    
+  }else
+  {
+    mostrarPop = true;
+    var botao = document.getElementById('botaoPop');
+    botao.click();
+  }
 }
-function disableScroll() {
-  // Get the current page scroll position
-  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  (scrollLeft = window.pageXOffset || document.documentElement.scrollLeft),
+
+var mostrarPop = respostaPopUp;
+function aceitarPopup(respostaPopUp)
+{
+  mostrarPop = respostaPopUp;
+  localStorage.setItem("mostrarPop",false);
+  return mostrarPop;
+}
+
+function disableScroll()
+{
+  mostrarPop = true;
+  mostrarPop = localStorage.getItem("mostrarPop");
+  if (mostrarPop)
+  {
+    
+  }else
+  {
+    mostrarPop = true;
+    // Get the current page scroll position
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    (scrollLeft = window.pageXOffset || document.documentElement.scrollLeft),
     // if any scroll is attempted,
     // set this to the previous value
     (window.onscroll = function () {
-      window.scrollTo(scrollLeft, scrollTop);
+    window.scrollTo(scrollLeft, scrollTop);
     });
+  } 
 }
 
-function enableScroll() {
+function enableScroll()
+{
   window.onscroll = function () {};
 }
+
 function encaminharSite()
 {
-  // window.close();
-   window.location.replace('/decline_popup.html');
-  
-}
-function encaminharSiteEn()
-{
-  // window.close();
-   window.location.replace('/en/decline_popup.html');
-  
+   window.location.replace('/pt-BR/decline_popup.html');
 }
 
-// function showPopup(){
-//   aux = localStorage.getItem("aux");
-//   localStorage.setItem("aux","changed");
-// }
+function encaminharSiteEn()
+{
+  window.location.replace('/decline_popup.html');
+}
+
+
