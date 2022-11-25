29b6fd4b8064921db77b500302c0b97893ac70a9
iomes2 <renaniomes10@gmail.com>
2022-11-25 10:25:11 -0300

    testandoRedirecionamento

--- js/detect-lang.js
+++ js/detect-lang.js
@@ -35,13 +35,13 @@ function changeLang()
     // || 'en-US' || 'en-EG' || 'en-AU' || 'en-GB' || 'en-CA' || 'en-NZ' || 'en-IE' || 'en-ZA' || 'en-JM' || 'en-BZ' || 'en-TT'
     else if(lang == 'en')
     {
-      window.location.replace('/index.html');
+      window.location.replace('/indexen.html');
       sessionStorage.setItem("alreadyChangeLang","true");
       // console.log("aba trocada para en");
     }
     else
     {
-      window.location.replace('/index.html');
+      window.location.replace('/indexen.html');
       sessionStorage.setItem("alreadyChangeLang","true");
       // console.log("linguagem desconhecida, encaminhando para en");
     }
