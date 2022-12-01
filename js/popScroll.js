7f523fbaa0e99c07721366e3845c239684d1274f
renan iomes <renaniomes10@gmail.com>
2022-12-01 09:41:33 -0300

    att 1-12-2022

--- js/popScroll.js
+++ js/popScroll.js
@@ -1,29 +1,24 @@
-$( document ).ready(function() {
-  function mostrarPopUp() {
-    mostrarPop = true;
-    mostrarPop = localStorage.getItem("mostrarPop");
-    mostrarCookie = localStorage.getItem("mostrarCookie");
-    // console.log("cookie está: " + mostrarCookie);
-    // console.log("mostrarPop está: " + mostrarPop);
-    // mostrarPop = true = nao aparecer
-    if (mostrarPop && mostrarCookie) {
-    } else {
-      ligarPressionarBotao();
-    }
+function mostrarPopUp() {
+  mostrarPop = true;
+  mostrarPop = localStorage.getItem("mostrarPop");
+  mostrarCookie = localStorage.getItem("mostrarCookie");
+  // console.log("cookie está: " + mostrarCookie);
+  // console.log("mostrarPop está: " + mostrarPop);
+  // mostrarPop = true = nao aparecer
+  if (mostrarPop && mostrarCookie) {
+  } else {
+    ligarPressionarBotao();
   }
-}); 
-
+}
 function ligarPressionarBotao(){
   let timeout;
   timeout = setTimeout(pressionarBotao,2000); 
 }
-
 function pressionarBotao(){
   mostrarPop = true;
   var botao = document.getElementById('botaoPop');
   botao.click();
 }
-
 function aceitarPopup(respostaPopUp)
 {
   console.log("mostrarPop agora é " + mostrarPop)
@@ -39,18 +34,15 @@ function aceitarPopup(respostaPopUp)
   }
   return mostrarPop;
 }
-
 // function mostrarAgebox(){
 //   document.getElementById('age-box').style.visibility='visible';
 // }
-
 function disableScroll()
 {
   mostrarPop = true;
   mostrarPop = localStorage.getItem("mostrarPop");
   if (mostrarPop)
   {
-    
   }else
   {
     mostrarPop = true;
@@ -64,24 +56,9 @@ function disableScroll()
     });
   } 
 }
-
-function enableScroll()
-{
-  window.onscroll = function () {};
-}
-
-function encaminharSite()
-{
-   window.location.replace('/pt-BR/decline_popup.html');
-}
-
-function encaminharSiteEn()
-{
-  window.location.replace('/en/decline_popup.html');
-}
-function encaminharSiteEs()
-{
-  window.location.replace('/es/decline_popup.html');
-}
+function enableScroll(){  window.onscroll = function () {};}
+function encaminharSite(){   window.location.replace('/pt-BR/decline_popup.html');}
+function encaminharSiteEn(){  window.location.replace('/en/decline_popup.html');}
+function encaminharSiteEs(){  window.location.replace('/es/decline_popup.html');}
 
 
